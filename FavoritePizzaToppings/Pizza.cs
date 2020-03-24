2ecf81f4f5314d727787ed0199092a3ce3e5f457
Connor Sullivan <cksullvn@gmail.com>
2020-03-23 21:21:52 -0500

    finished exercise

---
+++ FavoritePizzaToppings/Pizza.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoritePizzaToppings
+{
+    class Pizza
+    {
+        public List<string> Toppings { get; set; }
+
+    }
+}
